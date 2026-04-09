b2a2bb0adda0a531402a7e6b97ee818e87303a84
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-09 11:22:22 +0200

    Added snake keyboard input

--- src/SnakeGame/SnakeGame/Program.cs
+++ src/SnakeGame/SnakeGame/Program.cs
@@ -3,13 +3,19 @@
 int width = 20;
 int height = 10;
 
+int snakeX = 5;
+int snakeY = 5;
+
 Console.CursorVisible = false;
 
 for (int y = 0; y < height; y++)
 {
     for (int x = 0; x < width; x++)
     {
-        Console.Write("#");
+        if (x == snakeX && y == snakeY)
+            Console.Write("O"); 
+        else
+            Console.Write(".");
     }
     Console.WriteLine();
 }
