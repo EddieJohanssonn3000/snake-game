3e5ba53ebe9d2d902e0f7b701bcc15772e17dd8e
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-09 11:29:22 +0200

    Added structure and direction of the snake

--- src/SnakeGame/SnakeGame/Program.cs
+++ src/SnakeGame/SnakeGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 int width = 20;
 int height = 10;
@@ -6,16 +7,55 @@ int height = 10;
 int snakeX = 5;
 int snakeY = 5;
 
+int dirX = 1;
+int dirY = 0;
+
 Console.CursorVisible = false;
 
-for (int y = 0; y < height; y++)
+while (true)
 {
-    for (int x = 0; x < width; x++)
+    Console.Clear();
+
+    for (int y = 0; y < height; y++)
     {
-        if (x == snakeX && y == snakeY)
-            Console.Write("O"); 
-        else
-            Console.Write(".");
+        for (int x = 0; x < width; x++)
+        {
+            if (x == snakeX && y == snakeY)
+                Console.Write("O");
+            else
+                Console.Write(".");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
+
+    if (Console.KeyAvailable)
+    { 
+        var key = Console.ReadKey(true).Key;
+
+        if (key == ConsoleKey.UpArrow)
+        {
+            dirX = 0;
+            dirY = -1;
+        }
+        if (key == ConsoleKey.DownArrow)
+        {
+            dirX = 0;
+            dirY = 1;
+        }
+        if (key == ConsoleKey.LeftArrow)
+        {
+            dirX = -1;
+            dirY = 0;
+        }
+        if (key == ConsoleKey.RightArrow)
+        {
+            dirX = 1;
+            dirY = 0;
+        }
+    }
+    
+    snakeX += dirX;
+    snakeY += dirY;
+    
+    Thread.Sleep(200);
 }
